4405603a48d270e5745b7b9c3314a3a13de928c8
shoeomonkey <romanr32@gmail.com>
2014-02-27 20:30:20 +0100

    gtg

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120))
     password = db.Column(db.String(120))
+    profile = db.relationship('Profile', uselist=False, backref="users")
 
     def __init__(self, username, password):
         self.username = username
@@ -16,3 +17,17 @@ class Users(db.Model):
 
     def __str__(self):
         return self.username
+
+class Profile(db.Model):
+    __tablename__ = "profile"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200))
+    email = db.Column(db.String(100))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+
+    def __repr__(self):
+        return "<Profile(email='%s')>" % self.email
+
+    
